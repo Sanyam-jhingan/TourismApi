e2bd920586255d06ffc387485938b2ee449e0a5f
sanyam <sanyam.jhingan1.19@mountblue.tech>
2022-04-09 18:07:39 +0530

    add env variables

--- connection.js
+++ connection.js
@@ -1,10 +1,16 @@
 const { Client } = require('pg');
 
+const USER = process.env.USER || 'postgres';
+const PASSWORD = process.env.PASSWORD || 'postgres';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 5432;
+const DATABASE = process.env.DATABASE || 'postgres';
+
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'postgres',
+    user: USER,
+    host: HOST,
+    database: DATABASE,
+    password: PASSWORD,
     port: 5432,
 });
 
