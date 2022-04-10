8c450baee26b22ed1ff68738e537a3d5496c9455
sanyam <sanyam.jhingan1.19@mountblue.tech>
2022-04-10 18:14:21 +0530

    add access-control

--- api.js
+++ api.js
@@ -5,6 +5,25 @@ const bodyParser = require("body-parser")
 const app = express()
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  //res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 const PORT = process.env.PORT || 3300
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
